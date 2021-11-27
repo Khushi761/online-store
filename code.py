d66083e4e02db674ce9330006a97b2aa0f67cc0c
Michael-Jalloh <michaeljalloh19@gmail.com>
2021-11-27 20:13:34 +0000

    updates

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import csv      
 
 customer_titles = None
-customers = None
+customers = []
 products = []
 product_titles = None
 
@@ -16,35 +16,25 @@ def readfile(file_name):
     return (titles, data)
 
 
-
-# print()
-# product_titles, products = readfile("Products.csv")
-# print("Product Titles", product_titles)
-# print("Products", products)
-
 def get_customers():
     global customer_titles
     global customers
-    customer_titles, customers = readfile("Customers.csv")        
+    customers = []
+    customer_titles, customer_details = readfile("Customers.csv")        
 
-    customer_details = []
-    for customer in customers:
-        c = {"name": customer[0], "address": customer[1], "product": customer[2], "quantity": customer[3], "amount": customer[4]}
-        customer_details.append(c)
-    
-    #print(customer_details)
-
+    for customer in customer_details:
+        c = {"name": customer[0], "address": customer[1], "product": customer[2], "quantity": customer[3], "amount": int(customer[4])}
+        customers.append(c)
 
 def get_products():
     global product_titles
     global products
-    product_titles, product_detais = readfile("Products.csv")
+    products = []
+    product_titles, product_details = readfile("Products.csv")
 
-    for product in product_detais:
-        p = {"name":product[0],"price":product[1]}
+    for product in product_details:
+        p = {"name":product[0],"price":int(product[1])}
         products.append(p)
-    
-    #print(products)
 
 get_customers()
 
@@ -52,14 +42,18 @@ print()
 
 get_products()
 
-print(customers)
-print()
-print(products)
 
-# def total_amounts_due():
-#     customer()
-#     #product()
 
+
+def total_amounts_due():
+    get_customers()
+    get_products()
+
+    print(customers)
+    print()
+    print(products)
+
+total_amounts_due()
 #     # for x in cus:
 #     #     print()
 
